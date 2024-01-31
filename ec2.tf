6c8ab4735615e0c65d848ac8285f920aab3e1325
salmanrasheedmd <msalmanrasheed2000@gmail.com>
2024-01-31 21:53:08 +0530

    updated

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   key_name  = "terraform"
   subnet_id     = aws_subnet.ibm-web-sn.id
-  vpc_security_group_id = aws_security_group.ibm-db-nacl-sg.id
+  vpc_security_group_id = [aws_security_group.ibm-db-nacl-sg.id]
   user_data = file("ecomm.sh")
 
   tags = {
