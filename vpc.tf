4ee78dac64c960b7c3da1d67714e0d9307ab6df7
salmanrasheedmd <msalmanrasheed2000@gmail.com>
2024-01-31 19:33:57 +0530

    web subnet

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,16 @@ resource "aws_vpc" "myntra-vpc" {
     Name = "myntra"
   }
 }
+
+# web subnet
+
+resource "aws_subnet" "myntra-web-sn" {
+  vpc_id     = aws_vpc.myntra-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "Myntra-web-subnet"
+  }
+}
