00c6c6956b0cbbf69b8f104a55746af8563e6ce5
salmanrasheedmd <msalmanrasheed2000@gmail.com>
2024-01-31 20:31:25 +0530

    create web security

--- vpc.tf
+++ vpc.tf
@@ -146,3 +146,36 @@ resource "aws_network_acl_association" "myntra-db-nacl-asc" {
   network_acl_id = aws_network_acl.myntra-db-nacl.id
   subnet_id      = aws_subnet.myntra-db-sn.id
 }
+
+# web security group
+resource "aws_security_group" "myntra-web-nacl-sg" {
+  name        = "myntra-web-traffic"
+  description = "Allow SSH - HTTP inbound traffic"
+  vpc_id      = aws_vpc.myntra-vpc.id
+
+  ingress {
+    description = "SSH from WWW"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from WWW"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "myntra-web-sg"
+  }
+}
