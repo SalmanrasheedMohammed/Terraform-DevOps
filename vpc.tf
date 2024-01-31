54af1dbe2eaf8ada47284176ef911026fdf1e968
salmanrasheedmd <msalmanrasheed2000@gmail.com>
2024-01-31 20:14:05 +0530

    create web nacl

--- vpc.tf
+++ vpc.tf
@@ -80,3 +80,32 @@ resource "aws_route_table_association" "myntra-database-asc" {
   subnet_id      = aws_subnet.myntra-db-sn.id
   route_table_id = aws_route_table.myntra-database-rt.id
 }
+
+# web NACL
+
+resource "aws_network_acl" "myntra-web-nacl" {
+  vpc_id = aws_vpc.myntra-vpc.id
+  
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+
+
+  tags = {
+    Name = "myntra-web-nacl"
+  }
+}
